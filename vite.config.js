816bddd3e0a20ff1f75078a3caae8269dc8c2770
AF-pulse <patrik.hallen@dynaventures.se>
2026-03-07 11:46:54 +0100

    Add base path configuration for GitHub Pages
    Set base path for GitHub Pages and custom domain.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
+
+  base: "/",   // viktigt för GitHub Pages + custom domain
+
   server: {
     proxy: {
       "/api": {
